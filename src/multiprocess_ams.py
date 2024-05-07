87605c12ddea9aa5a9b3769980235c4c340b40f4
Pierre Laville <72388883+PierreLaville@users.noreply.github.com>
2024-05-07 10:44:28 +0200

    run multivcf_extract in multiprocessing

--- src/multiprocess_ams.py
+++ src/multiprocess_ams.py
@@ -1,12 +1,14 @@
 # coding:utf-8
 """
-Script to launch multiple ams pipeline processes
+Script to launch multiple AMS pipeline processes
+The AMS pipeline estimates the mismatch between a donor and a recipient based on VCF information
+command line help : python3 multiprocess_ams.py [-h]
 """
 import os
 import concurrent.futures
 import time
 import sys
-from tools import arguments_handling, multivcf_extract
+from tools import arguments_handling, multivcf_extract, table_operations
 
 # one argument function to be used by ProcessPoolExecutor.map multiprocessing function
 def launch_ams_pipeline(command_line):
@@ -22,6 +24,20 @@ def launch_ams_pipeline(command_line):
     # get donor and recipient names
     donor, recipient = command_line.split(" ")[2:4]
     return f"Done comparing the couple {donor} {recipient}"
+    
+def launch_multivcf_extract(command_line):
+    """
+    Returns a message saying the couple was successfully extracted, after having executed the command line
+    Parameters :
+                    command_line (str): command line of the multivcf extraction
+    Returns :
+                    (str): message saying the vcf were successfully extracted
+    """
+    # execute command line
+    os.system(command_line)
+    # get donor and recipient names
+    donor, recipient = command_line.split(" ")[-2:]
+    return f"Done extracting the couple {donor} {recipient}"    
 
 
 def read_pairs_info(donors_recipients_table, couples):
@@ -48,8 +64,8 @@ def read_pairs_info(donors_recipients_table, couples):
 
 def main():
     """
-    TODO
-    Runs the AMS pipeline estimates the mismatch between a donor and a recipient based on VCF information
+    Script to launch multiple AMS pipeline processes
+    The AMS pipeline estimates the mismatch between a donor and a recipient based on VCF information
     command line help : python3 multiprocess_ams.py [-h]
     """
     args = arguments_handling.arguments(sys.argv[1])
@@ -62,19 +78,46 @@ def main():
     
     # extract donor and recipient columns from multi VCF
     path_couples = []
+    path_out_couples = multivcf_extract.create_dependencies()
+    commands_multivcf = [
+    f"python3 tools/multivcf_extract.py {args.multi_vcf} {donor} {recipient}"
+    for (donor, recipient) in couples
+    ]
+    
     for (donor, recipient) in couples:
-        path_donor, path_recipient = multivcf_extract.main(args.multi_vcf, donor, recipient)
+        path_donor = path_out_couples + "/{}.vcf.gz".format(donor)
+        path_recipient = path_out_couples + "/{}.vcf.gz".format(recipient)
         path_couples.append((path_donor, path_recipient))
-        
-#   Doesn't take into account -wc
-#    f"-wc --wc_donor {args.wc_donor} --wc_recipient {args.wc_recipient}"
+    print(commands_multivcf)
+    
+    # start mesuring time
+    start = time.time()
+    # multiprocessing
+    COUNT = 0
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        # use all commands in list on the function
+        results = executor.map(launch_multivcf_extract, commands_multivcf)
+        for res in results:
+            print(res)
+            COUNT += 1
+            print(COUNT)
+            print(res)
+    # stop time count
+    end = time.time()
+    print("Elapsed time:", end - start, "seconds")
+    
+    # leading zeros for pairs ID : 01 instead of 1 (nb > 10), 001 instead of 1 (nb > 100)
+    leading_zeros_number = len(str(len(path_couples)))
     commands = [
     f"python3 ams_pipeline.py {path_donor} {path_recipient} {args.orientation} "
     f"--min_dp {args.min_dp} --max_dp {args.max_dp} --min_ad {args.min_ad} "
     f"--homozygosity_thr {args.homozygosity_thr} --gnomad_af {args.gnomad_af} "
     f"--min_gq {args.min_gq} --base_length {args.base_length} "
-    f"--run_name {args.run_name} --pair P{position}{full}"
-    for position, (path_donor, path_recipient) in enumerate(path_couples, start = 1)
+    f"--run_name {args.run_name} --pair P{pair_number:0{leading_zeros_number}d}"
+    f"{full} --norm_score"
+#   Doesn't take into account -wc
+#    f"-wc --wc_donor {args.wc_donor} --wc_recipient {args.wc_recipient}"
+    for pair_number, (path_donor, path_recipient) in enumerate(path_couples, 1)
     ]
     
     print("couples : ",len(couples))
